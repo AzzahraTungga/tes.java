c7713399bf7969fe4d52bf33172ff1a3461f09a1
AzzahraTungga <azzahra_dewi_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:40:02 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main(String[] args) {
+    //tipe_data nama_var = value/isi var;
+    String nama = "zea";
+    int usia = 15;
+    String alamat = "Jl. kauman dua";
+    double kelas = 10.7; 
+
+    System.out.println("Nama saya : " + nama);
+    System.out.println("usia :" + usia);
+    System.out.println("alamat :" + alamat);
+    System.out.println("kelas :" + kelas);
+    }
+}
