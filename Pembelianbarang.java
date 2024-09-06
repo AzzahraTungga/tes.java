3f4aeee40ef4bef983f28bb6a568ff5cc554237e
AzzahraTungga <azzahra_dewi_33rpl@student.smktelkom-mlg.sch.id>
2024-09-06 10:26:44 +0700

    Add files via upload

---
+++ Pembelianbarang.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pembelianbarang {
+    public static void main(String[] args) {
+        double total, diskon, pothar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan total pembelian: ");
+        total = input.nextDouble();
+
+        if (total > 150000) {
+            pothar =0;
+            diskon = 0.1 * total;
+            total -= diskon;
+        }   else { 
+            diskon = 0; 
+            pothar = 5000;
+            total  -= pothar;
+        }
+
+        System.out.println(" diskon: " + diskon);
+        System.out.println("potongan harga: " +pothar);
+        System.out.println("total yang harus dibayarkan: " + total);
+
+    }
+}
